faa44a334c30ab2da95b89c82485d0a70531b278
ffnuser <151765508+FFNUser@users.noreply.github.com>
2026-03-30 17:33:13 +0500

    Bump version from 1.4.5 to 1.4.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name="cyan-x",
-  version="1.4.5",
+  version="1.4.6",
   description="(blunt-rw) finally, pyzule doesn't suck",
   author="zx",
   author_email="z@zxcvbn.fyi",
